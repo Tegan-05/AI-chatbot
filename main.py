f0c1f421ec62f2b8b9e4243c414326cd0c6cb787
Tegan-05 <teganpillay69@gmail.com>
2026-06-27 00:58:35 +0200

    Feat: implement chatbot features and add gitignore

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+from langchain_core.messages import HumanMessage, HumnanMessage
+from langchain_google_genai import ChatGoogleGenAI
+from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+
+load_dotenv()
 def main():
-    print("Hello from ai-chatbot!")
+    model = ChatGoogleGenAI(
+        model="gemini-1.5-flash",
+        temperature=0,
+    )
+    tool = []
+    agent = create_react_agent(model=model, tools=tool)
+    print("Welcome to the AI Chatbot! Type 'exit' to quit.")
+    print("Lets preform calculations, answer questions, and more!")
+    user_input = input("\nYou: ").strip()
 
+    while True:
 
-if __name__ == "__main__":
-    main()
+        if user_input == "exit":
+            break
+
+        print("\nAssistant: ", end="")
+        for chunk in agent.stream(
+            {"messages": [HumanMessage(content=user_input)]}
+        ):
+            if "agent" in chunk and "messages" in chunk["agent"]:
+                for message in chunk["agent"]["messages"]:
+                    print(message.content, end="")
+
+    
